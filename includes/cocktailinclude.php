55e614a1ca9eda6aa2913ccddd247b58f245ffbf
DanielCH5 <danielc.hansen1@gmail.com>
2024-11-13 18:55:50 +0100

    Cleaned up code into includes

---
+++ includes/cocktailinclude.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="da">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Sigende titel</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<?php include("includes/navtop.php") ?>
+
+<div class="produktindhold">
+    <?php include("includes/navvertical.php") ?>
+
+
+    <div class="produktbillede">
+        <?php
+        echo '<img src="pics/cocktail' . $cocktail->cockId . '.jpg" class="w-100" alt="' . $cocktail->cockNavn . '">';
+        ?>    </div>
+    <div class="produkttekst">
+        <div class="ingredienser">
+            <h2>
+                <?php
+                echo $cocktail->cockNavn;
+                ?>
+            </h2>
+            <?php
+            echo $cocktail->cockIngredienser;
+            ?>
+        </div>
+
+        <div class="tekst">
+            <?php
+            echo $cocktail->cockBeskrivelse;
+            ?>
+        </div>
+        <div class="knapper"></div>
+    </div>
+    <div class="filtrering"></div>
+</div>
+
+<script src="app.js"></script>
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
+
+
