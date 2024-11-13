ab15920df9163ef5dbef13f286a77f1f3e02b031
DanielCH5 <danielc.hansen1@gmail.com>
2024-11-13 16:53:04 +0100

    Cocktail segment

--- index.php
+++ index.php
@@ -23,51 +23,14 @@ require "settings/init.php";
 
 <?php include("includes/navtop.php") ?>
 
-<div class="indhold">
+<div class="indexindhold">
     <?php include("includes/navvertical.php") ?>
 
 
-    <div class="indholdindhold">
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            <div class="col">
-                <div class="card h-100">
-                    <a href="produkt.php">
-                        <img src="pics/cocktail2.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Moscow Mule</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="pics/cocktail1.jpg" class="card-img-top h-100" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Pina Colada</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="pics/cocktail3.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Sex on the beach</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+    <div class="indeximg">
+        <img class="w-100" src="pics/drinks.webp" alt="">
     </div>
-    <div></div>
-
+</div>
     <script src="app.js"></script>
 
 
@@ -76,39 +39,3 @@ require "settings/init.php";
 </body>
 
 </html>
-<!-- Produkt Cards
-<div class="row g-2">
-	<?php
-	$produkter = $db->sql("SELECT * FROM produkter");
-	foreach($produkter as $produkt) {
-		?>
-		<div class="col-12 col-md-6">
-			<div class="card w-100">
-				<div class="card-header">
-					<?php
-					echo $produkt->prodNavn;
-					?>
-				</div>
-				<div class="card-body">
-					<?php
-					// Indsæt andet felt fra database
-					?>
-				</div>
-				<div class="card-footer text-muted">
-					<?php
-					// Indsæt andet felt fra database
-					?>
-				</div>
-			</div>
-		</div>
-		<?php
-	}
-	?>
-
-</div>
-
-
-<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
--->
