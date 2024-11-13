cccfcf5093168fc7657f992eefc28a6f26191a08
DanielCH5 <danielc.hansen1@gmail.com>
2024-11-13 21:12:23 +0100

    Titles

--- cocktails.php
+++ cocktails.php
@@ -8,7 +8,7 @@ require "settings/init.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Cocktails</title>
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 </head>
 <body>
